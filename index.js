2aa9fffb3259ce0037d6d71e997437ead4040ce6
subirdas29 <you@example.com>
2022-11-09 22:31:57 +0600

    get individual service

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //middlewares
@@ -28,6 +28,15 @@ async function run(){
             const query = {} ;
             const cursor = countriesCollection.find(query);
             const result= await cursor.toArray();
+            console.log(result)
+            res.send(result);
+        })
+
+       
+        app.get('/services/allcountries/:id',async(req,res)=>{
+            const id = req.params.id
+            const query = {_id:ObjectId(id)} ;
+            const result = await countriesCollection.findOne(query);
             console.log(result)
             res.send(result);
         })
