4b85b86ff037d24312144dc8bcfee65cf9cb24b7
subirdas29 <you@example.com>
2022-11-10 02:47:19 +0600

    myreviews added

--- index.js
+++ index.js
@@ -46,9 +46,25 @@ async function run(){
         app.post('/reviews',async(req,res)=>{
             const review = req.body
             const result =await reviewCollection.insertOne(review)
-           console.log(result)
+         
             res.send(result);
         })
+       
+
+        app.get('/reviews',async(req,res)=>{
+            let query = {}; 
+            if(req.query.email)
+            {
+                query ={
+                    email:req.query.email
+                }
+            }
+            const cursor= reviewCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+    
 
     }
     finally{
